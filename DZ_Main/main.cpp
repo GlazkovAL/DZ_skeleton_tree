c84490338f7c9a049287550cdb2fcbfdefbde21b
GlazkovAL <sashglz388@gmail.com>
2023-12-07 23:50:45 +0300

    Merge branch 'Blinkova'

--- DZ_Main/main.cpp
+++ DZ_Main/main.cpp
@@ -35,53 +35,54 @@ istream& operator >> (istream& in, Edge& ed) {
 
 //�������� �����
 void Prim(int n, vector <Edge> graph, string file_name) {
-	ofstream fout;
-	set <int> visited_p = {1};
-	vector <Edge> path = {};
-	int edges_in=0;
-	int active_p=1;
-	int min_weight;
-	int v_p;
-	Edge v_ed;
-	int sum_weight=0;
+    ofstream fout;
+    set <int> visited_p = { 1 };
+    vector <Edge> path = {};
+    int edges_in = 0;
+    int active_p = 1;
+    int min_weight;
+    int v_p;
+    Edge v_ed;
+    int sum_weight = 0;
 
-	auto start_Func = system_clock::now();
+    auto start_Func = system_clock::now();
 
-	while (edges_in != n-1) {
-		min_weight = 9999;
-		for (int point : visited_p) {
-			for (Edge ed : graph) {
-				if (((ed.start_p == point && visited_p.count(ed.end_p)==0) || (ed.end_p == point && visited_p.count(ed.start_p) == 0)) && ed.weight < min_weight) {
-					min_weight = ed.weight;
-					if (ed.start_p == point) v_p = ed.end_p;
-					else v_p = ed.start_p;
-					v_ed = ed;
-				}
-				
-			}
+    while (edges_in != n - 1) {
+        min_weight = 9999;
+        for (int point : visited_p) {
+            for (Edge ed : graph) {
+                if (((ed.start_p == point && visited_p.count(ed.end_p) == 0) || (ed.end_p == point && visited_p.count(ed.start_p) == 0)) && ed.weight < min_weight) {
+                    min_weight = ed.weight;
+                    if (ed.start_p == point) v_p = ed.end_p;
+                    else v_p = ed.start_p;
+                    v_ed = ed;
+                }
 
-		}
-		visited_p.insert(v_p);
-		path.push_back(v_ed);
-		sum_weight += min_weight;
-		edges_in += 1;
-	}
+            }
 
-	auto read_time = system_clock::now() - start_Func;
+        }
+        visited_p.insert(v_p);
+        path.push_back(v_ed);
+        sum_weight += min_weight;
+        edges_in += 1;
+    }
 
-	fout.open("Answers/"+file_name+".txt");
-	fout << "Minimal cost: " <<sum_weight << endl;
-	fout << "Time: " << duration_cast<microseconds>(read_time).count() << " Microseconds" << endl;
-	fout << "Path:" << endl;
+    auto read_time = system_clock::now() - start_Func;
 
-	if (fout.is_open()) {
-		for (int i = 0; i < n - 1; i++)
-		{
-			fout << path[i];
-		}
-	}
-	fout.close();
-// ������� ��� ������ ����� �������
+    fout.open("Answers/" + file_name + ".txt");
+    fout << "Minimal cost: " << sum_weight << endl;
+    fout << "Time: " << duration_cast<microseconds>(read_time).count() << " Microseconds" << endl;
+    fout << "Path:" << endl;
+
+    if (fout.is_open()) {
+        for (int i = 0; i < n - 1; i++)
+        {
+            fout << path[i];
+        }
+    }
+    fout.close();
+}
+
 int findRoot(vector<int>& parent, int vertex) {
     if (parent[vertex] == vertex) {
         return vertex;
@@ -103,7 +104,6 @@ void unionSets(vector<int>& parent, vector<int>& rank, int root1, int root2) {
     }
 }
 
-// �������� �������� � �������� ���������������� ��������
 void Kruskal(int n, vector<Edge> graph, string file_name) {
     ofstream fout;
     vector<Edge> path;
@@ -111,19 +111,17 @@ void Kruskal(int n, vector<Edge> graph, string file_name) {
     vector<int> rank(n + 1, 0);
     int edges_in = 0;
     int sum_weight = 0;
-    int start = clock();
 
-    // ������������� ��������� ������
+    auto start_Func = system_clock::now();
+
     for (int i = 1; i <= n; i++) {
         parent[i] = i;
     }
 
-    // ���������� ����� �� ����
     sort(graph.begin(), graph.end(), [](const Edge& a, const Edge& b) {
         return a.weight < b.weight;
         });
 
-    // ���������� ��������� ������
     for (Edge edge : graph) {
         int root1 = findRoot(parent, edge.start_p);
         int root2 = findRoot(parent, edge.end_p);
@@ -140,11 +138,10 @@ void Kruskal(int n, vector<Edge> graph, string file_name) {
         }
     }
 
-    int end = clock();
-    int t = (end - start);
+    auto read_time = system_clock::now() - start_Func;
     fout.open("Answers/" + file_name + ".txt");
     fout << "Minimal cost: " << sum_weight << endl;
-    fout << "Time: " << t << endl;
+    fout << "Time: " << duration_cast<microseconds>(read_time).count() << " Microseconds" << endl;
     fout << "Path:" << endl;
 
     if (fout.is_open()) {
@@ -161,7 +158,7 @@ int main() {
     vector<Edge> graph;
     Edge ed{ 0, 0, 0 };
 
-    // ������ ���� ������� ����
+
     fin.open("Tests/1test.txt");
     if (fin.is_open()) {
         fin >> n;
@@ -171,10 +168,10 @@ int main() {
         }
     }
     fin.close();
+    Prim(n, graph, "Test1/Prim_tr");
     Kruskal(n, graph, "Test1/Kruskal_tr");
 
     graph = {};
-    // ������ ���� ������ ����
     fin.open("Tests/2test.txt");
     if (fin.is_open()) {
         fin >> n;
@@ -184,11 +181,12 @@ int main() {
         }
     }
     fin.close();
+    Prim(n, graph, "Test2/Prim_tr");
     Kruskal(n, graph, "Test2/Kruskal_tr");
 
     graph = {};
 
-    // ������ ���� ����������� ����
+ 
     fin.open("Tests/3test.txt");
     if (fin.is_open()) {
         fin >> n;
@@ -198,6 +196,7 @@ int main() {
         }
     }
     fin.close();
+    Prim(n, graph, "Test3/Prim_tr");
     Kruskal(n, graph, "Test3/Kruskal_tr");
 
     return 0;
